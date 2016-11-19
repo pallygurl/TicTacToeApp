42d6168096cbf7968e531690c1cdbc3460fea184
Cyndi Bolton <cbolton@wvtpg.com>
2016-11-19 12:43:32 -0500

    most tests passing

--- unbeatable_ai.rb
+++ unbeatable_ai.rb
@@ -1,51 +1,58 @@
+# Win: If the player has two in a row, they can place a third to get three in a row.
+# Block: If the opponent has two in a row, the player must play the thrid themselves to block the opponent.
+# Fork: Create an opportunity where the player has two threats to win (two non-blocked lines of 2).
+# Blocking an opponent's fork:
+    # Option 1: The player should create two in a row to force the opponent into defending, as long as it doesn't result in them creating a fork. For example, if "X" has a corner, "O" has the center, and "X" has the opposite corner as well, "O" must not play a corner in order to win. (Playing a corner in this scenario creates a fork for "X" to win.)
+
+    # Option 2: If there is a configuration where the opponent can fork, the player should block that fork.
+
+# Center: A player marks the center. (If it is the first move of the game, playing on a corner gives "O" more opportunities to make a mistake and may therefore be the better choice; however, it makes no difference between perfect players.)
+
+# Opposite corner: If the opponent is in the corner, the player plays the opposite corner.
+# Empty corner: The player plays in a corner square.
+
+# Empty side: The player plays in a middle square on any of the 4 sides.
+
+
 class UnbeatableAI
     attr_accessor :marker, :open_spot
 
 
     def initialize(marker)
         @marker = marker
-        @open_spot = 10    
+        @open_spot = 10  
     end
     
 
     def get_move(board)
-        @open_spot = 10
         comp_marker = marker
 
-        if comp_marker == "o"
-            player_marker = "x"
+        if comp_marker == "O"
+            player_marker = "X"
         else
-            player_marker = "o"
+            player_marker = "O"
         end
 
         if potential_win_block(board, comp_marker) <= 8
-            move = open_spot
+            move = potential_win_block(board, comp_marker)
         elsif potential_win_block(board, player_marker) <= 8
-            move = open_spot
+             move = potential_win_block(board, player_marker)
         elsif check_for_center(board)
-            move = open_spot
+            move = check_for_center(board)
         elsif strategy_opposite_corners(board)
-            move = open_spot
+            move = strategy_opposite_corners(board)
         elsif strategy1_for_empty_corner(board)
-            move = open_spot
+            move = strategy1_for_empty_corner(board)
+        elsif check_for_fork(board) <= 8
+                move = check_for_fork(board)
+        elsif block_opponents_fork(board, comp_marker) <= 8
+                move = block_opponents_fork(board, comp_marker)
         else edge_space(board)
-            move = open_spot
+            move = edge_space(board)
         end
         move
-        # if check_win_block(board, comp_marker) <= 8
-        #     move = check_win_block(board, comp_marker)
-
-        #     elsif check_win_block(board, player_marker) <=8
-        #         move = check_win_block(board, player_marker)
-
-        #     elsif check_for_fork(board) <= 8
-        #         move = check_for_fork(board)
-
-        #     elsif block_opponents_fork(board, comp_marker) <= 8
-        #         move = block_opponents_fork(board, comp_marker)
-
-        #     elsif check_for_center(board)
-        #         move = check_for_center(board)
+       
+        
 
 		# 	elsif check_empty_corner(board)
 		# 		move = check_empty_corner(board)
@@ -73,14 +80,15 @@ class UnbeatableAI
     end
 
     def win_positions
-        win_positions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        win_position = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     end
 
     def potential_win_block(board, current_marker)
+        @open_spot = 10
 
-        win_combinations(board).each_with_index do |winner, index|
-            if winner.count(current_marker) == 2 && winner.count(" ") == 1
-                space_in_winning_line = winner.index(" ")
+        win_combinations(board).each_with_index do |win_line, index|
+            if win_line.count(current_marker) == 2 && win_line.count(" ") == 1
+                space_in_winning_line = win_line.index(" ")
                 @open_spot = win_positions[index][space_in_winning_line]
             end
         end
